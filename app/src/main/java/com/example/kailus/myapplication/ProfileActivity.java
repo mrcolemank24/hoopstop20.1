061288205738fa8fda7eeef84523b35875f3be8f
mrcolemank24 <mrcolemank24@gmail.com>
2019-01-28 19:43:00 -0600

    Initial commit

---
+++ app/src/main/java/com/example/kailus/myapplication/ProfileActivity.java
@@ -0,0 +1,98 @@
+package com.example.kailus.myapplication;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView profilePic;
+    private TextView profileName, profileAge, profileEmail;
+    private Button profileUpdate, changePassword;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        profilePic = findViewById(R.id.ivProfilePic);
+        profileName = findViewById(R.id.tvProfileName);
+        profileAge = findViewById(R.id.tvProfileAge);
+        profileEmail = findViewById(R.id.tvProfileEmail);
+        changePassword = findViewById(R.id.btnChangePassword);
+        profileUpdate = findViewById(R.id.btnProfileUpdate);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                profileName.setText("Name: " + userProfile.getUserName());
+                profileAge.setText("Age: " + userProfile.getUserAge());
+                profileEmail.setText("Email: " + userProfile.getUserEmail());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ProfileActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        profileUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    startActivity(new Intent(ProfileActivity.this, UpdateProfile.class));
+
+
+            }
+        });
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, UpdatePassword.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+              onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
